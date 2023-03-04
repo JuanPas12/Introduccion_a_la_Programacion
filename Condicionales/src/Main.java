244763f55c4cafe34a2c369c7e217a0ed48536eb
JuanPas12 <125917353+JuanPas12@users.noreply.github.com>
2023-03-04 12:27:04 -0600

    Add files via upload

---
+++ Condicionales/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String estacion = "verano";
+        if (estacion == "primavera"){
+            System.out.println("Es primavera");
+        }else if(estacion == "verano"){
+            System.out.println("Es verano");
+        }else {
+            System.out.println("Es otra estación");
+        }
+    }
+}
